8c6faf2a11e30b73e8f439da7a04c904057edac9
mengpeilee <asdfg3865@gmail.com>
2019-05-09 17:24:09 +0800

    UI enhancement

--- src/components/Recovery.js
+++ src/components/Recovery.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Actions } from 'react-native-router-flux';
 import Swiper from 'react-native-swiper';
+import { No2of2, No3of1, No4of2 } from '../image';
+
+const { width, height } = Dimensions.get('window');
 
 const styles = {
   containerStyle: {
@@ -26,39 +29,60 @@ const styles = {
     fontSize: 30,
     fontWeight: 'bold',
   },
+  image: {
+    flex: 1,
+    maxWidth: width,
+    resizeMode: 'contain',
+  },
   paginationStyle: {
     position: 'absolute',
     bottom: 10,
     right: 10,
   },
   paginationText: { fontSize: 20 },
+  contextStyle: {
+    fontSize: 20,
+    textAlign: 'left',
+  },
   barStyle: {
-    height: 90,
+    height: 150,
     shadowOpacity: 0.3,
     shadowOffset: { width: 0, height: 2 }, // 左右不要有陰影
     backgroundColor: '#fff',
   },
 };
 
-const renderPagination = (index, total, context) => {
-  return (
-    <View style={styles.barStyle}>
-      <View style={styles.paginationStyle}>
-        <Text style={{ color: 'grey' }}>
-          <Text style={styles.paginationText}>{index + 1}</Text>/{total}
-        </Text>
-      </View>
-    </View>
-  );
-};
-
 class Recovery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      context: '',
+      index: 0,
+      total: 0,
+    };
+  }
+
   componentDidMount() {
     console.log('test');
   }
 
+  renderPagination() {
+    const { barStyle, paginationStyle, paginationText, contextStyle } = styles;
+    const { context, index, total } = this.state;
+    return (
+      <View style={barStyle}>
+        <Text style={contextStyle}>{context}</Text>
+        <View style={paginationStyle}>
+          <Text style={{ color: 'grey' }}>
+            <Text style={paginationText}>{index}</Text>/{total}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+
   render() {
-    const { containerStyle, wrapper, slide, backStyle } = styles;
+    const { containerStyle, wrapper, slide, backStyle, text, image } = styles;
     return (
       <View style={containerStyle}>
         <TouchableOpacity
@@ -76,20 +100,33 @@ class Recovery extends Component {
         </TouchableOpacity>
         <Swiper
           style={wrapper}
-          renderPagination={renderPagination}
+          // renderPagination={this.pagination}
           showsButtons
           loop={false}
         >
           <View style={slide}>
-            <Text style={styles.text}>Test</Text>
+            <View>
+              <Image style={image} source={No2of2} />
+              {this.renderPagination()}
+            </View>
           </View>
           <View style={slide}>
-            <Text style={styles.text}>Test2</Text>
+            <View>
+              <Image style={image} source={No3of1} />
+              {this.renderPagination()}
+            </View>
           </View>
           <View style={slide}>
-            <Text style={styles.text}>Test2</Text>
+            <View>
+              <Image style={image} source={No3of1} />
+              {this.renderPagination()}
+            </View>
+          </View>
+          <View style={slide}>
+            <Text style={text}>恭喜完成！</Text>
           </View>
         </Swiper>
+        {this.renderPagination}
       </View>
     );
   }
