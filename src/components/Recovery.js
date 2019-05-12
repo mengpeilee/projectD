50ecbcc67d9afeea9ad79c991da6fdb68e613311
mengpeilee <asdfg3865@gmail.com>
2019-05-12 17:53:52 +0800

    make code more reuse

--- src/components/Recovery.js
+++ src/components/Recovery.js
@@ -3,7 +3,28 @@ import { View, Text, TouchableOpacity, Image, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Actions } from 'react-native-router-flux';
 import Swiper from 'react-native-swiper';
-import { No2of2, No3of1, No4of2 } from '../image';
+import {
+  No1of1,
+  No1of2,
+  No1of3,
+  No2of1,
+  No2of2,
+  No2of3,
+  No3of1,
+  No3of2,
+  No3of3,
+  No4of1,
+  No4of2,
+  No4of3,
+  No5of1,
+  No5of2,
+  No5of3,
+  No1gif,
+  No2gif,
+  No3gif,
+  No4gif,
+  No5gif,
+} from '../image';
 
 const { width } = Dimensions.get('window');
 
@@ -53,15 +74,74 @@ const styles = {
   },
 };
 
-const recoveryData = {
-  firstPracticeTitle: '孟德爾森練習\nstep1-3、共10次',
-  firstPracticeText1: '將您的手指放在喉結上',
-  firstPracticeText2: '做一個吞嚥的動作，並感受您的喉結上下移動',
-  firstPracticeText3:
-    '再吞嚥一次，用您的手向上推擠喉嚨的肌肉，讓喉結在上方停留三秒',
-};
+const recoveryData = [
+  {
+    title: '孟德爾森練習\nstep1-3、共10次',
+    first: '將您的手指放在喉結上',
+    second: '做一個吞嚥的動作，並感受您的喉結上下移動',
+    third: '再吞嚥一次，用您的手向上推擠喉嚨的肌肉，讓喉結在上方停留三秒',
+    endTitle: '完成10次後，請按按鈕前往下一個練習',
+    buttonText: '',
+    firstImage: No1of1,
+    secondImage: No1of2,
+    thirdImage: No1of3,
+    gif: No1gif,
+  },
+  {
+    title: '下巴伸展練習\nstep1-3、共10次',
+    first: '將您的頭向後傾，盡可能地張開嘴，越大越好，維持這樣的姿勢達五秒鐘',
+    second: '當您將頭後傾停留之時，將您的下巴向前延伸如圖所示',
+    third:
+      '維持頭後傾的姿勢，將您的下巴向上往鼻子移動試圖要將嘴巴閉起，此時您應該可以感受到脖子前面的部分正在伸展，這樣的姿勢維持五秒鐘',
+    endTitle: '完成10次後，請按按鈕前往下一個練習',
+    buttonText: '',
+    firstImage: No2of1,
+    secondImage: No2of2,
+    thirdImage: No2of3,
+    gif: No2gif,
+  },
+  {
+    title: '聲門上吞嚥練習\nstep1-3、共10次',
+    first: '由鼻子吸氣然後憋住這口氣',
+    second: '繼續憋氣，接著吞一口口水',
+    third: '用一個咳嗽或清喉嚨的動作來吐氣，然後再做一次吞嚥的動作',
+    endTitle: '完成10次後，請按按鈕前往下一個練習',
+    buttonText: '',
+    firstImage: No3of1,
+    secondImage: No3of2,
+    thirdImage: No3of3,
+    gif: No3gif,
+  },
+  {
+    title: '出力吞嚥\nstep1-3、共10次',
+    first: '將您的舌頭向上顎盡可能地用力頂住',
+    second:
+      '舌頭位於同樣位置時，盡可能地用喉嚨的力量用力吞一口口水，使用喉嚨的肌肉用力擠壓',
+    third: '吞嚥後即可放鬆',
+    endTitle: '完成10次後，請按按鈕前往下一個練習',
+    buttonText: '',
+    firstImage: No4of1,
+    secondImage: No4of2,
+    thirdImage: No4of3,
+    gif: No4gif,
+  },
+  {
+    title: '馬沙可夫練習\nstep1-3、共10次',
+    first: '將您的舌頭伸出在嘴唇或牙齒之間',
+    second: '維持舌頭的姿勢，然後嘗試吞一口口水',
+    third: '吞嚥後即可放鬆',
+    endTitle: '完成10次後，請按按鈕前往下一個練習',
+    buttonText: '',
+    firstImage: No5of1,
+    secondImage: No5of2,
+    thirdImage: No5of3,
+    gif: No5gif,
+  },
+];
 
 class Recovery extends Component {
+  state = { practiceNum: 1, swiperData: recoveryData[0] };
+
   componentDidMount() {
     console.log('test');
   }
@@ -82,6 +162,7 @@ class Recovery extends Component {
 
   render() {
     const { containerStyle, wrapper, slide, backStyle, text, image } = styles;
+    const { practiceNum, swiperData } = this.state;
     return (
       <View style={containerStyle}>
         <TouchableOpacity
@@ -107,34 +188,38 @@ class Recovery extends Component {
           }}
         >
           <View style={slide}>
-            <Text style={text}>{recoveryData.firstPracticeTitle}</Text>
+            <Text style={text}>{swiperData.title}</Text>
           </View>
           <View style={slide}>
             <View>
-              <Image style={image} source={No2of2} />
-              {this.renderPagination(1, 3, recoveryData.firstPracticeText1)}
+              <Image style={image} source={swiperData.firstImage} />
+              {this.renderPagination(1, 3, swiperData.first)}
             </View>
           </View>
           <View style={slide}>
             <View>
-              <Image style={image} source={No3of1} />
-              {this.renderPagination(2, 3, recoveryData.firstPracticeText2)}
+              <Image style={image} source={swiperData.secondImage} />
+              {this.renderPagination(2, 3, swiperData.second)}
             </View>
           </View>
           <View style={slide}>
             <View>
-              <Image style={image} source={No4of2} />
-              {this.renderPagination(3, 3, recoveryData.firstPracticeText3)}
+              <Image style={image} source={swiperData.thirdImage} />
+              {this.renderPagination(3, 3, swiperData.third)}
             </View>
           </View>
           <View style={slide}>
             <Text
               style={text}
               onPress={() => {
+                this.setState({
+                  swiperData: recoveryData[practiceNum],
+                  practiceNum: practiceNum + 1,
+                });
                 this.swiper.scrollBy(-4, false);
               }}
             >
-              請重複10次！
+              {swiperData.endTitle}
             </Text>
           </View>
         </Swiper>
