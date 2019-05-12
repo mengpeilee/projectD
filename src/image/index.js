15c119b3f68a898611b7382c50ab944d80a4512b
mengpeilee <asdfg3865@gmail.com>
2019-05-12 15:32:19 +0800

    add image and gif

--- src/image/index.js
+++ src/image/index.js
@@ -1,5 +1,43 @@
+import No1of1 from './1-1.png';
+import No1of2 from './1-2.png';
+import No1of3 from './1-3.png';
+import No2of1 from './2-1.png';
 import No2of2 from './2-2.png';
+import No2of3 from './2-3.png';
 import No3of1 from './3-1.png';
+import No3of2 from './3-2.png';
+import No3of3 from './3-3.png';
+import No4of1 from './4-1.png';
 import No4of2 from './4-2.png';
+import No4of3 from './4-3.png';
+import No5of1 from './5-1.png';
+import No5of2 from './5-2.png';
+import No5of3 from './5-3.png';
+import No1gif from './1.gif';
+import No2gif from './2.gif';
+import No3gif from './3.gif';
+import No4gif from './4.gif';
+import No5gif from './5.gif';
 
-export { No2of2, No3of1, No4of2 };
+export {
+  No1of1,
+  No1of2,
+  No1of3,
+  No2of1,
+  No2of2,
+  No2of3,
+  No3of1,
+  No3of2,
+  No3of3,
+  No4of1,
+  No4of2,
+  No4of3,
+  No5of1,
+  No5of2,
+  No5of3,
+  No1gif,
+  No2gif,
+  No3gif,
+  No4gif,
+  No5gif,
+};
