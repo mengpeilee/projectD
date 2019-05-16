881a1658609bee37aed576f3af026a68b0195f93
mengpeilee <asdfg3865@gmail.com>
2019-05-16 13:34:11 +0800

    start UI

--- src/components/SceneRouter.js
+++ src/components/SceneRouter.js
@@ -4,13 +4,15 @@ import HomePage from './HomePage';
 import Recovery from './Recovery';
 import RankingList from './RankingList';
 import PersonalInformation from './PersonalInformation';
+import Start from './Start';
 
 class RouterComponent extends Component {
   render() {
     return (
       <Router>
         <Scene key="root" initial>
-          <Scene key="homePage" component={HomePage} initial hideNavBar />
+          <Scene key="start" component={Start} initial hideNavBar />
+          <Scene key="homePage" component={HomePage} hideNavBar />
           <Scene key="recovery" component={Recovery} hideNavBar />
           <Scene key="rankingList" component={RankingList} hideNavBar />
           <Scene
---
+++ src/components/Start.js
@@ -0,0 +1,169 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Image,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  Platform,
+  TextInput,
+} from 'react-native';
+import { logo, NYMULogo } from '../image';
+
+const { width, height } = Dimensions.get('window');
+
+const secondColor = '#1A71B7'; // 藍色
+
+const styles = {
+  containerStyle: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  headerContainer: {
+    height: 50,
+    justifyContent: 'center',
+  },
+  headerStyle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: secondColor,
+    alignSelf: 'center',
+  },
+  inputContainer: {
+    flex: 1,
+    paddingLeft: 30,
+    paddingRight: 30,
+    justifyContent: 'center',
+  },
+  inputStyle: {
+    flex: 1,
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 15,
+    ...Platform.select({
+      ios: { fontFamily: 'PingFang TC' },
+      android: {
+        fontFamily: 'sans-serif',
+        paddingBottom: 8,
+      },
+    }),
+  },
+  topArea: {
+    flex: 2,
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  inputArea: {
+    flex: 5,
+    justifyContent: 'flex-start',
+  },
+  inputDiv: { height: 45 },
+  imageStyle: {
+    width: 100,
+    height: 100,
+    paddingHorizontal: 80,
+    alignSelf: 'center',
+    resizeMode: 'contain',
+  },
+  button: { alignSelf: 'flex-end' },
+  loginBtn: {
+    justifyContent: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    minWidth: 100,
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: '#ED2A67',
+    backgroundColor: '#ED2A67',
+  },
+  loginTextStyle: {
+    fontSize: 14,
+    alignSelf: 'center',
+  },
+};
+
+class Start extends Component {
+  state = {
+    idText: '',
+    nameText: '',
+  };
+
+  inputText = () => {
+    const { idText, nameText } = this.state;
+    const {
+      topArea,
+      imageStyle,
+      inputDiv,
+      inputStyle,
+      button,
+      loginBtn,
+      loginTextStyle,
+      inputContainer,
+      inputArea,
+    } = styles;
+    return (
+      <View style={inputContainer}>
+        <View style={topArea}>
+          <Image style={imageStyle} source={logo} />
+          <Image style={imageStyle} source={NYMULogo} />
+        </View>
+        <View style={inputArea}>
+          <View style={inputDiv}>
+            <TextInput
+              placeholder="請輸入身分證字號"
+              placeholderTextColor="#ccc"
+              autoCorrect={false}
+              style={inputStyle}
+              value={idText}
+              autoCapitalize="none"
+              onChangeText={text => this.setState({ idText: text })}
+              underlineColorAndroid="transparent"
+            />
+          </View>
+          <View style={inputDiv}>
+            <TextInput
+              placeholder="請取一個屬於你的暱稱"
+              placeholderTextColor="#ccc"
+              autoCorrect={false}
+              style={inputStyle}
+              value={nameText}
+              autoCapitalize="none"
+              onChangeText={text => this.setState({ nameText: text })}
+              underlineColorAndroid="transparent"
+              // onSubmitEditing={onSubmitEditing}
+            />
+          </View>
+          <View style={button}>
+            <TouchableOpacity
+              onPress={() => console.log('onPress')}
+              style={loginBtn}
+            >
+              <Text style={loginTextStyle}>登入</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  renderHeader = () => {
+    const { headerContainer, headerStyle } = styles;
+    return (
+      <View style={headerContainer}>
+        <Text style={headerStyle}>頭頸吞嚥復健APP</Text>
+      </View>
+    );
+  };
+
+  render() {
+    const { containerStyle } = styles;
+    return (
+      <View style={containerStyle}>
+        {this.renderHeader()}
+        {this.inputText()}
+      </View>
+    );
+  }
+}
+
+export default Start;
